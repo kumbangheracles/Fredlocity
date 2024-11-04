2bbe37821963d4774953ecb34dae3276495d04c4
kumbangheracles <kumbangheracles@gmail.com>
2024-11-04 11:58:24 +0700

    header fixed

--- scripts.js
+++ scripts.js
@@ -109,17 +109,17 @@ navigation.forEach(function(el){
 })
 
 // Service Worker
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
-        .then((registration) => {
-          console.log('Service Worker registered with scope: ', registration.scope);
-        })
-        .catch((error) => {
-          console.log('Service Worker registration failed: ', error);
-        });
-    });
-  }
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', () => {
+//       navigator.serviceWorker.register('/service-worker.js')
+//         .then((registration) => {
+//           console.log('Service Worker registered with scope: ', registration.scope);
+//         })
+//         .catch((error) => {
+//           console.log('Service Worker registration failed: ', error);
+//         });
+//     });
+//   }
 
   AOS.init({
     offset: 100
